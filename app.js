21aa2ea0e8b66468730e5c6ffe38a59f9dab9119
GUNNER9285 <GUNNER9285@gmail.com>
2019-01-09 15:54:20 +0700

    delete error handle

--- app.js
+++ app.js
@@ -15,24 +15,6 @@ app.use(bodyParser.json());
 require('./models/temperature.model');
 require('./routes/index.route')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
 var port = process.env.PORT||3000;
 app.listen(port);
 console.log("Server running at ", port);
