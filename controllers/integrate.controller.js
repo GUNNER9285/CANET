b95ae6e4b8af713a838308ca60b2e1ceee63698c
GUNNER9285 <GUNNER9285@gmail.com>
2019-01-10 15:05:03 +0700

    fix API saveBeacon

--- controllers/integrate.controller.js
+++ controllers/integrate.controller.js
@@ -15,48 +15,55 @@ exports.getBeacon = function(req, res) {
 exports.saveBeacon = function(req, res) {
     var beacon = req.body['beacon'];
     console.log(beacon);
-    var datetime = beacon['datetime'].split(" "); // [0] = date, [1] = time
-    var find = "";
-    db.beaconData.find({
-        date: datetime[0]
-    }, function (err, docs) {
-        if (docs.length != 0) {
-            console.log('found');
-            var hours = parseInt(datetime[1][0]+datetime[1][1]);
-            console.log(hours);
-            var times = docs[0]['time'];
-            times[hours] = (parseInt(times[hours]) + 1) + '';
-            db.beaconData.update({
-                date: datetime[0]
-            },{
-                $set:{
-                    time: times
-                }
-            }, function (err, docs) {
-                if (docs != null) {
-                    console.log('found', JSON.stringify(docs));
-                    res.json(docs);
+    if(beacon['status'] == 'enter'){
+        var datetime = beacon['datetime'].split(" "); // [0] = date, [1] = time
+        db.beaconData.find({
+            date: datetime[0]
+        }, function (err, docs) {
+            if (docs.length != 0) {
+                console.log('found');
+                var hours = parseInt(datetime[1][0]+datetime[1][1]);
+                console.log(hours);
+                var times = docs[0]['time'];
+                times[hours] = (parseInt(times[hours]) + 1) + '';
+                db.beaconData.update({
+                    date: datetime[0]
+                },{
+                    $set:{
+                        time: times
+                    }
+                }, function (err, docs) {
+                    if (docs != null) {
+                        console.log('found', JSON.stringify(docs));
+                        res.json(docs);
+                    } else {
+                        res.send('User not found');
+                    }
+                });
+            } else {
+                console.log('beaconData not found');
+                var hours = parseInt(datetime[1][0]+datetime[1][1]);
+                var times = ["0","0","0","0","0","0",
+                    "0","0","0","0","0","0",
+                    "0","0","0","0","0","0",
+                    "0","0","0","0","0","0"];
+                if(hours[0] == "0"){
+                    times[parseInt(datetime[1][1])] = "1";
                 } else {
-                    res.send('User not found');
+                    times[hours] = "1";
                 }
-            });
-        } else {
-            console.log('beaconData not found');
-            var hours = parseInt(datetime[1][0]+datetime[1][1]);
-            var times = ["0","0","0","0","0","0",
-                        "0","0","0","0","0","0",
-                        "0","0","0","0","0","0",
-                        "0","0","0","0","0","0"];
-            times[hours] = "1";
-            db.beaconData.insert({
-                date: datetime[0],
-                time: times
-            }, function (err, docs) {
-                console.log(docs);
-                res.send(docs);
-            });
-        }
-    });
+                db.beaconData.insert({
+                    date: datetime[0],
+                    time: times
+                }, function (err, docs) {
+                    console.log(docs);
+                    res.send(docs);
+                });
+            }
+        });
+    } else{
+        res.send('You leave.');
+    }
 };
 
 exports.showBeacon = function(req, res) {
